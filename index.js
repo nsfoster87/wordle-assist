b5373e889f9a3a979faf73228628cec7a855f63f
Nathaniel Foster <nsfoster87@yahoo.com>
2022-02-17 08:28:28 -0800

    Fix the keyboard keys so that they take the color of highest precedence if there are multiples of the same letter.
    Take away the 'tbd' state for letters

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const board = document.getElementById('board');
     const boardContainer = document.getElementById('board-container');
+    const tiles = document.querySelectorAll('.tile');
+
+    let currentTile = document.querySelector('[data-state="empty"]');
+    let previousTile = null;
 
     function resizeBoard() {
         if (boardContainer.offsetHeight >= 420) {
@@ -20,38 +24,56 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function toggleColor(e) {
         const clickedTile = e.target;
-        console.log(clickedTile);
         if (clickedTile.dataset.state === 'empty') {
             return;
         }
         clickedTile.dataset.animation = 'flip-in';
-
     }
 
-    const tiles = document.querySelectorAll('.tile');
-    let currentTile = document.querySelector('[data-state="empty"]');
-    let previousTile;
+    function updateKeyColor(key) {
+        const associatedTiles = Array.from(tiles).filter(tile => tile.textContent == key.textContent);
+        if (associatedTiles.length == 1) {
+            key.dataset.state = associatedTiles[0].dataset.state;
+        } else {
+            // correct letters should take precendence over 'present' letters
+            // and present letters should take precendence over 'absent' letters
+            let finalState = 'absent';
+            associatedTiles.forEach(tile => {
+                if (tile.dataset.state == 'correct') {
+                    finalState = 'correct';
+                }
+                if (finalState != 'correct') {
+                    if (tile.dataset.state == 'present') {
+                        finalState = 'present';
+                    }
+                    if (finalState != 'present') {
+                        if (tile.dataset.state == 'absent') {
+                            finalState = 'absent';
+                        }
+                    }
+                }
+            });
+            key.dataset.state = finalState;
+        }
+    }
+
     tiles.forEach(tile => tile.addEventListener('click', toggleColor));
     tiles.forEach(tile => tile.addEventListener('animationend', (e) => {
         const letter = e.target.textContent;
         const keyboardKey = document.querySelector(`#keyboard [data-key="${letter}"]`);
         if (e.target.dataset.animation === 'flip-in') {
             switch (e.target.dataset.state) {
-                case 'tbd':
-                    e.target.dataset.state = 'absent';
-                    keyboardKey.dataset.state = 'absent';
-                    break;
                 case 'absent':
                     e.target.dataset.state = 'present';
-                    keyboardKey.dataset.state = 'present';
+                    updateKeyColor(keyboardKey);
                     break;
                 case 'present':
                     e.target.dataset.state = 'correct';
-                    keyboardKey.dataset.state = 'correct';
+                    updateKeyColor(keyboardKey);
                     break;
                 case 'correct':
-                    e.target.dataset.state = 'tbd';
-                    keyboardKey.dataset.state = '';
+                    e.target.dataset.state = 'absent';
+                    updateKeyColor(keyboardKey);
                     break;
                 default:
                     break;
@@ -63,8 +85,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }));
 
     function calculateWordList() {
-        console.log('enter');
-
         const lastRow = previousTile.parentNode;
         lastRow.dataset.locked = 'true';
         lastRow.dataset.full = 'true';
@@ -72,67 +92,11 @@ document.addEventListener('DOMContentLoaded', function() {
         currentRow.dataset.locked = 'false';
 
         const completeWordRows = document.querySelectorAll('[data-full="true"]');
-        console.log(completeWordRows);
 
         let completeWords = [];
         completeWordRows.forEach(row => completeWords.push(Array.from(row.children)));
-        console.log(completeWords);
 
-        // each object in letters is a letter with a status and position in word:
-        // {
-        //  "j": {
-        //          status: correct,
-        //          position: 2
-        //       },
-        //  "l": {
-        //          status: absent,
-        //       }
-        //  "n": {
-        //          status: present,
-        //          not: [0, 2, 4]
-        //       }
-        // }
-        // needs to be tweaked. May be better to have positions as the objects
-        // and then a separate object of presentLetters and the positions they're
-        // not in
-
-        // let letters = {};
-        // completeWords.forEach(word => {
-        //     word.forEach((tile, index) => {
-        //         const letter = tile.textContent;
-        //         console.log(letter);
-        //         // if letter object doesn't exist, create one.
-        //         if (!letters[letter]) { letters[letter] = {}; }
-        //
-        //         // set the status of the letter object to the tile status
-        //         letters[letter].status = tile.dataset.state;
-        //
-        //         // set the position of the letter object if correct
-        //         // and if incorrect, add to the 'not' position array
-        //         if (letters[letter].status == 'correct') {
-        //             letters[letter].position = index;
-        //         } else if (letters[letter].status == 'present') {
-        //             if (!letters[letter].not) { letters[letter].not = []; }
-        //             letters[letter].not.push(index);
-        //         }
-        //     });
-        // });
-        // console.log(letters);
-
-        // let masterWord = {};
-        // {
-        //     0: {
-        //         solved: "a",
-        //     },
-        //     1: {
-        //         solved: "p",
-        //     },
-        //     2: {
-        //         not: ["e"],
-        //     },
-        //     present: ["e", "p"]
-        //     absent: ["r", "s", "i", "f"]
-        // }
+        // masterword object
         let masterWord = {
             0: {
                 solved: "",
@@ -157,9 +121,9 @@ document.addEventListener('DOMContentLoaded', function() {
             present: [],
             absent: []
         };
+
         completeWords.forEach(word => {
             const wordArray = word.map(tile => tile.textContent);
-            console.log(wordArray);
 
             word.forEach((tile, index) => {
                 let doubleLetter = false;
@@ -187,23 +151,12 @@ document.addEventListener('DOMContentLoaded', function() {
             })
         });
         console.log(masterWord);
-
-        // const wordTiles = Array.from(lastRow.children);
-        // let word = wordTiles.reduce((word, tile) => {
-        //     word += tile.textContent;
-        //     return word;
-        // }, '');
-        // console.log(word);
-
     }
 
     function enterLetter(e) {
         const key = e.target.dataset.key;
-        // needs to be updated to be able to grab from previous row
-        // console.log(currentTile);
-        // console.log(previousTile);
+        const keyboardKey = document.querySelector(`#keyboard [data-key="${key}"]`);
         if (key === "←") {
-            console.log('backspace pressed');
             if (previousTile) {
                 if (previousTile.parentNode.dataset.locked === 'false') {
                     const keyboardKey = document.querySelector(`#keyboard [data-key="${previousTile.textContent}"]`);
@@ -213,6 +166,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             }
         } else if (key === "↵") {
+            // only allow enter to react if user has filled entire row
             if (previousTile && previousTile === previousTile.parentNode.lastElementChild) {
                 calculateWordList();
             }
@@ -220,9 +174,12 @@ document.addEventListener('DOMContentLoaded', function() {
             if (currentTile.parentNode.dataset.locked === "false") {
                 currentTile.textContent = key;
                 currentTile.dataset.animation = "pop";
-                currentTile.dataset.state = 'tbd';
+                currentTile.dataset.state = 'absent';
+                updateKeyColor(keyboardKey);
             }
         }
+
+        // calculate the new previousTile and currentTile
         const filledTiles = document.querySelectorAll('.tile:not([data-state="empty"])');
         previousTile = filledTiles[filledTiles.length - 1];
         currentTile = document.querySelector('[data-state="empty"]');
@@ -230,4 +187,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const keys = document.querySelectorAll('#keyboard button');
     keys.forEach(key => key.addEventListener('click', enterLetter));
+
+    // TODO
+    // add functionality for typing
 });
